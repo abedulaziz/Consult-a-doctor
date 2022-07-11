aef30ba65d3184a7b681c9998be65f9536b737c5
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-12 01:05:30 +0300

    Backend: tiny edit in doctor_specific and user models

--- consult-a-doctor_backend/app/Models/Doctor_specific.php
+++ consult-a-doctor_backend/app/Models/Doctor_specific.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Doctor_specific extends Model
 {
     use HasFactory;
+
+    public function getDoctorSpecific() {
+        return $this->hasOne(User::class, "id");
+    }
 }
