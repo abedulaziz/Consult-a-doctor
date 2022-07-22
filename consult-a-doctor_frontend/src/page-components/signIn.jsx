855a403d2bb1a5bd3f05626c72c6b02e538f3b71
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-22 10:08:37 +0300

    Frontend: applying sign-in validation on inputs

--- consult-a-doctor_frontend/src/page-components/signIn.jsx
+++ consult-a-doctor_frontend/src/page-components/signIn.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import {Link} from 'react-router-dom';
+import {useForm} from 'react-hook-form';
 
 // layout components
 import RegistrationLeftSection from '../layout-components/registrationLeftSection';
 
 
 const SignIn = () => {
+
+  const {register, handleSubmit, formState: {errors}} = useForm();
+
   return (
     <div className='background'>
       <div className="sign-in_box regis_box">
@@ -20,12 +24,23 @@ const SignIn = () => {
           </div>
 
 
-          <form id='signIn' className='regis_form'>
-            <div className="email_wrapper">
-              <input type="text" name="email" id="email" placeholder='email' />
+          <form onSubmit={handleSubmit((data) => console.log(data))} id='signIn' className='regis_form'>
+            <div className="email">
+
+              <div className="email_wrapper">
+                <input {...register("email", {required: "Email is required", pattern:{value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, message:"Invalid email"}})}  type="text" name="email" id="email" placeholder='email' />
+                <p className="error">{errors.email?.message}</p>
+              </div>
+
             </div>
-            <div className="password_wrapper">
-              <input type="password" name="password" id="password" placeholder='password' />
+
+            <div className="password">
+            
+              <div className="password_wrapper">
+                <input {...register("password", {required: "Password is required", pattern:{value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm, message:"Please enter a valid password"}})} type="password" name="password" id="password" placeholder='password' />
+                <p className="error">{errors.password?.message}</p>
+              </div>
+
             </div>
             <div className="button_wrapper">
               <button className="log-in_button regis_button">Log in</button>
