6e90684f9dffcabdca35f3811430e3d7a47cb865
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-23 17:17:18 +0300

    Frontend: getting user appointments functionality

--- consult-a-doctor_frontend/src/page-components/appointments.jsx
+++ consult-a-doctor_frontend/src/page-components/appointments.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 // layout components
-import Header from '../layout-components/header';
-import Footer from '../layout-components/footer';
+import Header from "../layout-components/header";
+import Footer from "../layout-components/footer";
 
 // layout components
-import Appointment from '../layout-components/appointment';
+import Appointment from "../layout-components/appointment";
 
 const Appointments = () => {
+  const [appoinAsDoctor, setAppoinAsDoctor] = React.useState(null);
+  const [appoinAsPatient, setAppoinAsPatient] = React.useState(null);
+
+  const userID = useSelector((state) => state.userInfo.user_id);
+  console.log(userID);
+
+  React.useEffect(() => {
+    try {
+      const getAppointments = async () => {
+        const appoinRqust = await axios.get(`/users/${userID}/appointments`);
+
+        console.log(appoinRqust);
+        setAppoinAsDoctor(appoinRqust.data.appointmentsAsDoctor);
+        setAppoinAsPatient(appoinRqust.data.appointmentsAsPatient);
+      };
+      getAppointments();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
     <>
       <Header />
@@ -15,26 +38,37 @@ const Appointments = () => {
       <div className="appointments">
         <div className="container">
           <div className="appoin_content">
-
             <div className="heading">
               <h2>My Appointments</h2>
             </div>
 
             <div className="appoin-cards">
-              <Appointment doctor_name="John Doe" duration="11:00 - 13:00" date="Feb 17, 2022" />
-              <Appointment doctor_name="Mickel Smith" duration="14:00 - 14:30" date="March 14, 2022" />
-              <Appointment doctor_name="Wallac Johnson" duration="01:00 - 03:00" date="March 5, 2022" />
-              <Appointment doctor_name="Wan Bissaka" duration="12:00 - 13:00" date="Feb 22, 2022" />
-              <Appointment doctor_name="Marco Asensio" duration="20:00 - 21:00" date="December 17, 2022" />
+              {appoinAsDoctor &&
+                appoinAsDoctor.map((appointment) => (
+                  <Appointment
+                    key={appointment.id}
+                    doctor_name={appointment.fname + " " + appointment.lname}
+                    duration={appointment.from.substring(-3) + " - " + appointment.to.substring(-3)}
+                    date={appointment.date}
+                  />
+                ))}
+              {appoinAsPatient &&
+                appoinAsPatient.map((appointment) => (
+                  <Appointment
+                    key={appointment.id}
+                    doctor_name={appointment.fname + " " + appointment.lname}
+                    duration={appointment.from.substring(-3) + " - " + appointment.to.substring(-3)}
+                    date={appointment.date}
+                  />
+                ))}
             </div>
-
           </div>
         </div>
       </div>
 
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Appointments
+export default Appointments;
