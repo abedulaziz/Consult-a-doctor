04b640f5a2cba021195139d8426d2f6e01ffbe07
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-25 13:52:48 +0300

    Frontend: enhancing the structure of notifications component

--- consult-a-doctor_frontend/src/WebRTC-components/notifications.jsx
+++ consult-a-doctor_frontend/src/WebRTC-components/notifications.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import { SocketContext} from './SocketContext';
+
 const Notifications = () => {
+  const { answerCall, call, callAccepted } = React.useContext(SocketContext)
+
   return (
     <div>
-      Notifications
+        <div className="notifications">
+          <h3>{call.name} is calling...</h3>
+          <button id='answerCall' onClick={answerCall}>
+            Answer
+          </button>
+        </div>
     </div>
   )
 }
