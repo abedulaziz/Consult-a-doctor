a162e3d18eacc562fc324bae8a1f2c4efc5d352f
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-25 17:08:44 +0300

    Frontend: store video widget on a separate component

--- consult-a-doctor_frontend/src/page-components/meeting.jsx
+++ consult-a-doctor_frontend/src/page-components/meeting.jsx
@@ -11,20 +11,17 @@ const Meeting = () => {
       <div className="screens">
         <div className="screens_wrapper">
 
-          <div className="video_wrapper">
-            <video className='video_player' id='user-1' src="" autoPlay playsInline></video>
-          </div>
-          <div className="video_wrapper">
-            <video className='video_player' id='user-2' src="" autoPlay playsInline></video>
-          </div>
+          <VideoPlayer />
 
         </div>
+        
+        <Options>
+          <Notifications />
+        </Options>
       </div>
 
-      <VideoPlayer />
-      <Options>
-        <Notifications />
-      </Options>
+
+
     </>
   )
 }
