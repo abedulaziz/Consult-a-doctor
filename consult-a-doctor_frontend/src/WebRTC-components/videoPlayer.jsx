43bc5880cec8f94d6be1bf5c2ba70e1e98c2840c
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-25 10:13:58 +0300

    Frontend: structure of screen videos

--- consult-a-doctor_frontend/src/WebRTC-components/videoPlayer.jsx
+++ consult-a-doctor_frontend/src/WebRTC-components/videoPlayer.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {useSelector, useDispatch} from 'react-redux';
+
+import { SocketContext } from './SocketContext';
 
 const VideoPlayer = () => {
+  const { name, callAccepted, myVideo, userVideo, callEnded, call} = useContext(SocketContext)
+
   return (
-    <div>VideoPlayer</div>
+    <div className='videos_container'>
+      <div className="my-video">
+        <p className="name">{call.name || "Name"}</p>
+        <div className="video_wrapper">
+          <video ref={myVideo} playsInline autoPlay muted ></video>
+        </div>
+      </div>
+
+      <div className="user-video">
+        <p className="name">{call.name || "Name"}</p>
+        <div className="video_wrapper">
+          <video ref={userVideo} playsInline autoPlay muted ></video>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
