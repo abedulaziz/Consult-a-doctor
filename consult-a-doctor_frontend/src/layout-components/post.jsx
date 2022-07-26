f9861fe93dd6bf414c660f78f094d7933a64eea5
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-26 16:09:47 +0300

    Frontend: adding create_at to Post component

--- consult-a-doctor_frontend/src/layout-components/post.jsx
+++ consult-a-doctor_frontend/src/layout-components/post.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 
-const Post = ({profilePic, fullName}) => {
+const Post = ({profilePic, fullName, content, createdAt}) => {
   return (
     <div className='post'>
       <div className="heading">
         <div className="profile_pic">
           <img src={ profilePic } />
         </div>
-        <div className="fullname">{ fullName }</div>
+        <div className="fullname">
+          { fullName }
+          <div className="created_at">
+          {createdAt}
+        </div>
+        </div>
+
       </div>
 
       <div className="content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora similique nihil itaque harum earum dolor cupiditate iusto hic assumenda provident nostrum quibusdam, delectus laboriosam recusandae quasi vel asperiores sed.
+        {content}
+      </div>
+      <div className="created_at">
+  
       </div>
     </div>
   )
