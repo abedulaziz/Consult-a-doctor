289421f6fb911d1715baf9bc4c0fcdf3afe9db74
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-27 03:06:46 +0300

    Frontend: implementing add new blog to the database functionality and attach canceling and submitting event listeners to popup buttons

--- consult-a-doctor_frontend/src/helper-components/addBlogPopup.jsx
+++ consult-a-doctor_frontend/src/helper-components/addBlogPopup.jsx
@@ -1,24 +1,52 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import {setPopupVisibility} from '../redux/slices/addBlogPopupSlice'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setPopupVisibility } from "../redux/slices/addBlogPopupSlice";
+
+import axios from "axios";
 
 const AddBlogPopup = () => {
-  const popupVisibility = useSelector(state => state.addBlogPupop.value.PopupVisibitlityClass)
-  console.log(popupVisibility);
-  const dispatch = useDispatch()
+  const popupVisibility = useSelector((state) => state.addBlogPupop.value.PopupVisibitlityClass);
+  const userInfo = useSelector((state) => state.userInfo.value);
+  const dispatch = useDispatch();
+  const blogContent = React.useRef(null);
+
+  const submitBlog = async () => {
+    console.log(blogContent.current.value.length);
+    if (blogContent.current.value.length !== 0) {
+      try {
+        let rqustBody = { content: blogContent.current.value };
+        const doctorInfoRqust = await axios.post(`users/${userInfo.user_id}/add-blog`, rqustBody, {
+          headers: {
+            Authorization: `Bearer ${userInfo.JWT}`,
+          },
+        });
+        console.log(doctorInfoRqust.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    blogContent.current.focus();
+  };
 
   return (
-    <div className={`cd-popup ${popupVisibility}`} >
+    <div className={`cd-popup ${popupVisibility}`}>
       <div className="cd-popup-container">
-        <p>Are you sure you want to delete this element?</p>
+        <div className="blog-content">
+          <h3 className="new_blog_header">Blog content</h3>
+          <textarea ref={blogContent} name="content" id="content"></textarea>
+        </div>
         <ul className="cd-buttons">
-          <li><a href="#">Submit</a></li>
-          <li><a href="#">Cancel</a></li>
+          <li onClick={() => submitBlog()}>
+            <a href="#">Submit</a>
+          </li>
+          <li onClick={() => dispatch(setPopupVisibility(""))}>
+            <a href="#">Cancel</a>
+          </li>
         </ul>
-        <a href="#" className="cd-popup-close img-replace" onClick={() =>dispatch(setPopupVisibility(""))}></a>
+        <a href="#" className="cd-popup-close img-replace" onClick={() => dispatch(setPopupVisibility(""))}></a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddBlogPopup
+export default AddBlogPopup;
