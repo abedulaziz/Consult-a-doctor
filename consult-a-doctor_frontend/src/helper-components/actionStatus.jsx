f1772614533e5d11c136ff4f4bf23ec9290f5653
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-29 16:37:17 +0300

    Frontend: creating actionStatus component to track some http responses and behave accordingly

---
+++ consult-a-doctor_frontend/src/helper-components/actionStatus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActionStatus = () => {
+  return (
+    <div className='action-status' id='actionStatus'>
+      <p className="message">  </p>
+    </div>
+  )
+}
+
+export default ActionStatus
