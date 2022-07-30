3450fdc5f4a78a82b7121ae8208a8387713485f7
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-30 20:42:26 +0300

    Backend: adding deleteSpecialization and addNewSpecialization APIs in SpecializationsController

--- consult-a-doctor_backend/app/Http/Controllers/SpecializationsController.php
+++ consult-a-doctor_backend/app/Http/Controllers/SpecializationsController.php
@@ -3,16 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Specializations;
+use App\Models\Specialization;
 
 class specializationsController extends Controller
 {
     public function getSpecializations() {
 
-        $specializations = Specializations::select("id", "name", "background_image")->get();
-        
+        $specializations = Specialization::withCount("specialityDoctors")->get();
+
         return response()->json([
             "specializations" => $specializations
+        ]);
+    }
+
+    // add new specialization
+    public function addNewSpecialization(Request $request) {
+        Specialization::create([
+            "name" => $request->name,
+            "background_image" => $request->background_image
+        ]);
+
+        return response()->json([
+            "messaege" => "Specialization added successfully"
+        ]);
+    }
+
+    // delete existing specialization
+    public function deleteSpecialization(Request $request) {
+        Specialization::where([
+            "id" => $request->id
+        ])->delete();
+
+        return response()->json([
+            "messaege" => "Specialization deleted successfully"
         ]);
     }
 }
