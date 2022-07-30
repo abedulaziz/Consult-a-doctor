bfbbd5f83075e1c47ce0e0b8062741be86613266
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-30 16:01:57 +0300

    Frontend: styling specializations table in adminSpecializations admin component and importing sidebar in doctorRequests admin component

--- consult-a-doctor_frontend/src/admin/doctorRequests.jsx
+++ consult-a-doctor_frontend/src/admin/doctorRequests.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Sidebar from './layout/sidebar';
 
 const DoctorRequests = () => {
   return (
-    <div>dfsa</div>
+    <div className="admin_background">
+
+    <div className='admin_spec'>
+      <Sidebar />
+
+    
+
+    </div>
+  </div>
   )
 }
 
