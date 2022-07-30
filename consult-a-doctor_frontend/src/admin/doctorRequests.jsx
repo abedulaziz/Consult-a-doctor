c8b2a3abcd18fa5ba6be52051da6de7b4559e804
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-30 23:49:52 +0300

    Frontend: get doctor account requests from the backend and display to the UI in doctorRequests admin component

--- consult-a-doctor_frontend/src/admin/doctorRequests.jsx
+++ consult-a-doctor_frontend/src/admin/doctorRequests.jsx
@@ -1,18 +1,71 @@
-import React from 'react'
-import Sidebar from './layout/sidebar';
+import React, { useState, useEffect } from "react";
+import Sidebar from "./layout/sidebar";
+import Header from "./layout/header";
+
+import AccountRequest from "./admin_helper_components/accountRequest";
+
+import axios from "axios";
 
 const DoctorRequests = () => {
-  return (
-    <div className="admin_background">
+   const [accountRequest, setAccountRequest] = useState(null);
 
-    <div className='admin_spec'>
-      <Sidebar />
+   useEffect(() => {
+      try {
+         const getAccountRequests = async () => {
+            const doctorAccountRqust = await axios.get("/users/get-doctor-account-requests");
+            console.log(doctorAccountRqust);
 
-    
+            const accountRequests = doctorAccountRqust.data.account_requests;
+            setAccountRequest(accountRequests);
+         };
+         getAccountRequests();
+      } catch (err) {
+         console.log(err);
+      }
+   }, []);
+   return (
+      <div className="admin_background">
+         <Sidebar />
+         <div className="container">
+            <Header />
 
-    </div>
-  </div>
-  )
-}
+            <div className="content_table">
+               <h3 className="table_header">Doctor account requests</h3>
+               <table>
+                  <thead>
+                     <tr>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Email</th>
+                        <th>Gender</th>
+                        <th>Age</th>
+                        <th>Speciality</th>
+                        <th>About</th>
+                        <th>University</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {accountRequest &&
+                        accountRequest.map((req) => (
+                           <AccountRequest
+                              key={req.id}
+                              ID={req.id}
+                              fname={req.fname}
+                              lname={req.lname}
+                              email={req.email}
+                              gender={req.gender}
+                              date_of_birth={req.date_of_birth}
+                              speciality={req.speciality}
+                              about={req.about}
+                              university={req.university}
+                           />
+                        ))}
+                  </tbody>
+               </table>
+            </div>
+         </div>
+      </div>
+   );
+};
 
-export default DoctorRequests
+export default DoctorRequests;
