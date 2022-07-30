26e6a7cf2da352b513480015836d2b5684f022f6
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-30 20:44:24 +0300

    Backend: creating specialityDoctors function to get specialization doctors inside Specialization model

--- consult-a-doctor_backend/app/Models/Specialization.php
+++ consult-a-doctor_backend/app/Models/Specialization.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Doctor_specific;
 
-class Specializations extends Model
+class Specialization extends Model
 {
     use HasFactory;
+
+    public function specialityDoctors() {
+
+    return $this->hasMany(Doctor_specific::class, "speciality_id");
+    }
 }
