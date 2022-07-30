2d2905605a2259732e18b34b8899361ab9c12995
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-07-30 12:55:25 +0300

    Frontend: adding admin_search.svg icon to icons folder, deleting adminSpec component, and modifying admin header component structure

--- consult-a-doctor_frontend/src/admin/layout/header.jsx
+++ consult-a-doctor_frontend/src/admin/layout/header.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 
 // icons
-import {ReactComponent as SearchIcon} from '../../assets/icons/search.svg';
+import {ReactComponent as SearchIcon} from '../../assets/icons/admin_search.svg';
 
 const Header = () => {
   return (
     <div className='header'>
-      <div className="options">
 
-        <div className="search-box">
-          <SearchIcon />
-          {/* <input onChange={(ev) => setSearchValue(ev.target.value)} type="text" placeholder="Search" /> */}
-          
+      <div className="head_container">
+        <div className="options">
+
+          <div className="search-box">
+            <SearchIcon />
+            <input type="text" name="search" id="search" placeholder='Search' />
+            {/* <input onChange={(ev) => setSearchValue(ev.target.value)} type="text" placeholder="Search" /> */}
+
+          </div>
+
         </div>
+        <div className="profile_info">
 
-      </div>
-      <div className="profile_info">
+          <p className="admin_name">John Doe</p>
+          <div className="profile_pic_wrapper">
+            <img src="" alt="" />
+          </div>
 
-        <p className="admin_name">John Doe</p>
-        <img src="" alt="" />
+        </div>
 
       </div>
     </div>
