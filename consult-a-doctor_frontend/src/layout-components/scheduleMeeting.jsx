4120385a00078ab9b60e82f73c2daf88b1cbce7d
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-08-03 23:33:50 +0300

    Frontend: importing bookMeeting redux state in scheduleMeeting component to control book meeting popup visibility and functionality

--- consult-a-doctor_frontend/src/layout-components/scheduleMeeting.jsx
+++ consult-a-doctor_frontend/src/layout-components/scheduleMeeting.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import { changePopupVisib } from "../redux/slices/bookMeetingSlice";
 
-const ScheduleMeeting = ({doctor_fullname}) => {
+
+const ScheduleMeeting = () => {
+  
+  const bookMeeting = useSelector((state) => state.bookMeeting.value);
+  const dispatch = useDispatch()
+
   return (
-    <div className="layer">
+    <div className={"layer " + (bookMeeting.isPopupActive && " active")}>
 
       <div className='schedule-meeting'>
         <div className="popup_header">
           <h2 className="heading">Schedule a meeting</h2>
-          <p>schedule a real-time video chat meeting with ${doctor_fullname}</p>
+          <p>schedule a meeting with <span style={{color: "var(--main-theme-color)"}}>{bookMeeting.doctorFullname}</span></p>
         </div>
 
         <div className="meeting_date">
@@ -15,7 +22,7 @@ const ScheduleMeeting = ({doctor_fullname}) => {
           <input type="date" name="meeting_date" id="meetingDate" />
         </div>
 
-        <a href="#" className="cd-popup-close img-replace"></a>
+        <a href="#" className="cd-popup-close img-replace" onClick={() => dispatch(changePopupVisib(false))}></a>
         <div className="submit-date">
           <button>Next</button>
         </div>
