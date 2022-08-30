1ffa686cac8183232ce89b8b654a7684377c4dce
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-08-30 16:41:51 +0300

    Frontend: fixing displaying profile picture of a doctor in doctors page component and reflect the changes in doctorCard layout component

--- consult-a-doctor_frontend/src/layout-components/doctorCard.jsx
+++ consult-a-doctor_frontend/src/layout-components/doctorCard.jsx
@@ -1,40 +1,39 @@
-import React from 'react'
+import React from "react";
 
-import {Link} from 'react-router-dom';
+import DefaultPicture from "../assets/backgrounds/default_profile_picture.svg";
+import { Link } from "react-router-dom";
 
 // icons
-import {ReactComponent as Star} from '../assets/icons/star.svg';
-import {ReactComponent as PersonCheck} from '../assets/icons/PersonCheck.svg';
+import { ReactComponent as Star } from "../assets/icons/star.svg";
+import { ReactComponent as PersonCheck } from "../assets/icons/PersonCheck.svg";
 
-const DoctorCard = ({doctor_id, profile_pic, fullname, followers}) => {
-  return (
-    <div className='card'>
+const DoctorCard = ({ doctor_id, profile_pic, fullname, followers }) => {
+   return (
+      <div className="card">
+         <div className="img_wrapper">
+            <img src={profile_pic ? profile_pic : DefaultPicture} />
+         </div>
 
-      <div className="img_wrapper">
-        <img src={ profile_pic } />
+         <div className="doctor-info">
+            <h4 className="fullname">{fullname}</h4>
+            <div className="rate">
+               <Star />
+               <Star />
+               <Star />
+               <Star />
+               <Star />
+            </div>
+            <div className="followers">
+               <PersonCheck />
+               followers: {followers}
+            </div>
+
+            <div className="profile_link">
+               <Link to={`/doctor/${doctor_id}/profile`}>Go to profile</Link>
+            </div>
+         </div>
       </div>
+   );
+};
 
-      <div className="doctor-info">
-        <h4 className="fullname">{ fullname }</h4>
-        <div className="rate">
-          <Star />
-          <Star />
-          <Star />
-          <Star />
-          <Star />
-        </div>
-        <div className="followers">
-          <PersonCheck />
-          followers: { followers }
-        </div>
-
-        <div className="profile_link">
-          <Link to={`/doctor/${doctor_id}/profile`} >Go to profile</Link>
-        </div>
-      </div>
-
-    </div>
-  )
-}
-
-export default DoctorCard
+export default DoctorCard;
--- consult-a-doctor_frontend/src/page-components/doctors.jsx
+++ consult-a-doctor_frontend/src/page-components/doctors.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import Loader from '../helper-components/loader';
-import DefaultPicture from '../assets/backgrounds/default_profile_picture.svg';
+import Loader from "../helper-components/loader";
 
 import { t } from "i18next";
 
@@ -11,83 +10,95 @@ import Header from "../layout-components/header";
 import Footer from "../layout-components/footer";
 import DoctorCard from "../layout-components/doctorCard";
 
-
 // icons
 import { ReactComponent as SearchIcon } from "../assets/icons/search.svg";
 
-
 const Doctors = () => {
-  const [loader, setLoader] = React.useState(<Loader />);
-  const [doctors, setDoctors] = React.useState(null);
-  const [searchValue, setSearchValue] = React.useState("")
+   const [loader, setLoader] = React.useState(<Loader />);
+   const [doctors, setDoctors] = React.useState([]);
+   const [searchValue, setSearchValue] = React.useState("");
 
-  let { specialization_id } = useParams();
+   let { specialization_id } = useParams();
 
-  React.useEffect(() => {
-    try {
-      const getSpecDoctors = async () => {
-        const doctorsRqust = await axios.get(`/${specialization_id}/doctors`);
-        setDoctors(doctorsRqust.data.doctors);
-        setLoader(null)
-      };
-      getSpecDoctors();
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
+   React.useEffect(() => {
+      try {
+         const getSpecDoctors = async () => {
+            const doctorsRqust = await axios.get(`/${specialization_id}/doctors`);
+            setDoctors(doctorsRqust.data.doctors);
+            setLoader(null);
+         };
+         getSpecDoctors();
+      } catch (err) {
+         console.log(err);
+      }
+   }, []);
 
+   const sortDoctors = () => {};
 
+   return (
+      <>
+         {loader}
+         <Header />
 
-  return (
-    <>
-      {loader}
-      <Header />
+         <main>
+            <div className="doctors">
+               <div className="container">
+                  <div className="doctors_content">
+                     <div className="heading">
+                        <h2>{t("lang.doctors.header")}</h2>
+                        <p>{t("lang.doctors.desc")}</p>
+                     </div>
 
-      <main>
-        <div className="doctors">
-          <div className="container">
-            <div className="doctors_content">
-              <div className="heading">
-                <h2>{t("lang.doctors.header")}</h2>
-                <p>{t("lang.doctors.desc")}</p>
-              </div>
+                     <div className="search-panel">
+                        <div className="search-box">
+                           <SearchIcon />
+                           <input
+                              onChange={(ev) => setSearchValue(ev.target.value)}
+                              type="text"
+                              placeholder={t("lang.doctors.search_panel.search_box_placeholder")}
+                           />
+                        </div>
 
-              <div className="search-panel">
-                <div className="search-box">
-                  <SearchIcon />
-                  <input onChange={(ev) => setSearchValue(ev.target.value)} type="text" placeholder={t("lang.doctors.search_panel.search_box_placeholder")} />
-                </div>
+                        <div className="sorting">
+                           <label htmlFor="sortDoctors">{t("lang.doctors.search_panel.sorting_sec.label")}</label>
+                           <select name="sorting" id="sortDoctors" onChange={(ev) => sortDoctors(ev)}>
+                              <option value="alphabetically">{t("lang.doctors.search_panel.sorting_sec.options.option_1")}</option>
+                              <option value="rating">{t("lang.doctors.search_panel.sorting_sec.options.option_2")}</option>
+                              <option value="rating">{t("lang.doctors.search_panel.sorting_sec.options.option_3")}</option>
+                           </select>
+                        </div>
+                     </div>
 
-                <div className="sorting">
-                  <label htmlFor="sortDoctors">{t("lang.doctors.search_panel.sorting_sec.label")}</label>
-                  <select name="sorting" id="sortDoctors" onChange={(ev) => sortDoctors(ev)}>
-                    <option value="alphabetically">{t("lang.doctors.search_panel.sorting_sec.options.option_1")}</option>
-                    <option value="rating">{t("lang.doctors.search_panel.sorting_sec.options.option_2")}</option>
-                    <option value="rating">{t("lang.doctors.search_panel.sorting_sec.options.option_3")}</option>
-                  </select>
-                </div>
-              </div>
-
-              <div className="doctor-cards">
-
-                {
-                  doctors && doctors.filter(doctor => {
-                  let fullname = doctor.fname + " " + doctor.lname;
-                  return fullname.match(new RegExp(searchValue, "i"))
-                })
-                .map(doctor => 
-                  <DoctorCard doctor_id={doctor.doctor_id} key={doctor.doctor_id} profile_pic={doctor.profile_pic ? doctor.profile_pic : DefaultPicture} fullname={doctor.fname + " " + doctor.lname} followers="23" />
-                )}
-
-              </div>
+                     <div className="doctor-cards">
+                        {doctors.length ? (
+                           doctors
+                              .filter((doctor) => {
+                                 let fullname = doctor.fname + " " + doctor.lname;
+                                 return fullname.match(new RegExp(searchValue, "i"));
+                              })
+                              .map((doctor) => (
+                                 <DoctorCard
+                                    doctor_id={doctor.doctor_id}
+                                    key={doctor.doctor_id}
+                                    profile_pic={doctor.profile_pic_uri}
+                                    fullname={doctor.fname + " " + doctor.lname}
+                                    followers="23"
+                                 />
+                              ))
+                        ) : (
+                           <div className="fallback_text">
+                              <p>No doctors available for this specialization.</p>
+                           </div>
+                        )}
+                     </div>
+                  </div>
+               </div>
             </div>
-          </div>
-        </div>
-      </main>
+         </main>
 
-      <Footer />
-    </>
-  );
+         <Footer />
+      </>
+   );
 };
 
 export default Doctors;
