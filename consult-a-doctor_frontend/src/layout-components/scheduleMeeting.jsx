c86fddd3a277ab94451772dd928bbba313f93e89
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-09-02 16:50:01 +0300

    Frontend: modifying the UI structure and enhancing validation and appointment submission in scheduleMeeting layout component

--- consult-a-doctor_frontend/src/layout-components/scheduleMeeting.jsx
+++ consult-a-doctor_frontend/src/layout-components/scheduleMeeting.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setBookMeetingPopup } from "../redux/slices/popupControllerSlice";
-
-import TimeDuration from "../helper-components/timeDuration";
 
 import axios from "axios";
 import { useForm } from "react-hook-form";
 
 import { t } from "i18next";
-import jwt_decode from 'jwt-decode';
+import jwt_decode from "jwt-decode";
 
 import message from "../helper-components/message";
 import DefaultProfilePic from "../assets/backgrounds/default_profile_picture.svg";
@@ -16,9 +14,10 @@ import DefaultProfilePic from "../assets/backgrounds/default_profile_picture.svg
 const ScheduleMeeting = ({ doctor_id, doctor_profile_pic, doctor_fullname }) => {
    const weekDays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
    const [bookMeetingStage, setBookMeetingStage] = useState(1);
-   const [availableTimes, setAvailableTimes] = useState(null);
-   const [scheduledTimes, setScheduledTiems] = useState(null);
-   const [selectedDuration, setSelectedDuration] = useState(null);
+   const [availableTimes, setAvailableTimes] = useState([]);
+
+   const [selectedDuration, setSelectedDuration] = useState(undefined);
+   const [selectedTime, setSelectedTime] = useState(undefined);
 
    const [workingWeekDays, setWorkingWeekDays] = useState([]);
 
@@ -50,7 +49,7 @@ const ScheduleMeeting = ({ doctor_id, doctor_profile_pic, doctor_fullname }) => 
       const weekDay = weekDays[new Date(data.meeting_date).getDay()];
 
       try {
-         const workPeriodsRqust = await axios.post(
+         const availabilitiesRqust = await axios.post(
             `users/${doctor_id}/check-availability`,
             {
                week_day: weekDay,
@@ -63,8 +62,9 @@ const ScheduleMeeting = ({ doctor_id, doctor_profile_pic, doctor_fullname }) => 
             }
          );
 
-         setAvailableTimes(workPeriodsRqust.data.available_periods);
-         setScheduledTiems(workPeriodsRqust.data.other_appointments);
+         console.log(availabilitiesRqust.data);
+
+         setAvailableTimes(availabilitiesRqust.data.available_periods);
          setBookMeetingStage(2);
       } catch (err) {
          message(err.response.data.message);
@@ -72,48 +72,44 @@ const ScheduleMeeting = ({ doctor_id, doctor_profile_pic, doctor_fullname }) => 
    };
 
    const submitData = async (data) => {
-      if (selectedDuration) {
-         const timeTo = new Date(new Date(data.meeting_date + " " + data.meeting_time).getTime() + 5 * 60000).toLocaleTimeString("en-GB", { hour12: false });
+      if (selectedDuration && selectedTime) {
+         const timeTo = new Date(new Date(data.meeting_date + " " + selectedTime).getTime() + parseInt(selectedDuration) * 60000).toLocaleTimeString("en-GB", {
+            hour12: false,
+         });
+         const meetingData = {
+            doctor_id,
+            patient_id: userInfo.sub,
+            title: data.meeting_title,
+            date: data.meeting_date,
+            from: selectedTime,
+            to: timeTo,
+            duration: selectedDuration,
+         };
+
          try {
-            const confirmMeeting = await axios.post(
-               `meetings/appointments/set-appointment`,
-               {
-                  doctor_id,
-                  patient_id: userInfo.sub,
-                  title: data.meeting_title,
-                  date: data.meeting_date,
-                  from: data.meeting_time,
-                  to: timeTo,
-                  duration: selectedDuration,
+            const confirmMeeting = await axios.post(`meetings/appointments/set-appointment`, meetingData, {
+               headers: {
+                  Authorization: `Bearer ${localStorage.getItem("JWT")}`,
                },
-               {
-                  headers: {
-                     Authorization: `Bearer ${localStorage.getItem("JWT")}`,
-                  },
-               }
-            );
+            });
             message(confirmMeeting.data.message, false);
             dispatch(setBookMeetingPopup(null));
          } catch (err) {
             message(err.response.data.message);
          }
       } else {
-         message("Please select a duration");
+         message(`Please select a ${!selectedDuration ? "duration" : "time"}`);
       }
    };
 
-   const handleDurationClicked = (ev) => {
+   const handleItemClicked = (ev, setItem, setItemValue) => {
       const target = ev.currentTarget;
       const siblings = target.parentElement.childNodes;
 
-      for (let sibling of siblings) {
-         sibling.style.backgroundColor = "rgb(221, 220, 220)";
-         sibling.style.color = "#000";
-      }
+      for (let sibling of siblings) sibling.classList.remove("active");
+      target.classList.add("active");
 
-      target.style.backgroundColor = "var(--main-theme-color)";
-      target.style.color = "#FFF";
-      setSelectedDuration(target.dataset.duration);
+      setItem(setItemValue);
    };
 
    return (
@@ -127,7 +123,10 @@ const ScheduleMeeting = ({ doctor_id, doctor_profile_pic, doctor_fullname }) => 
 
                <div className="popup_header">
                   <h2 className="heading">{t("lang.popups.schedule_meeting.header")}</h2>
-                  <p className="doctor_name">{t("lang.popups.schedule_meeting.pre-name")}{doctor_fullname}</p>
+                  <p className="doctor_name">
+                     {t("lang.popups.schedule_meeting.pre-name")}
+                     {doctor_fullname}
+                  </p>
                </div>
                <div className="content_wrapper">
                   {bookMeetingStage == 1 ? (
@@ -157,59 +156,55 @@ const ScheduleMeeting = ({ doctor_id, doctor_profile_pic, doctor_fullname }) => 
                      </form>
                   ) : (
                      <form onSubmit={handleSubmit((data) => submitData(data))} className="meeting_submission">
-                        <h4 className="heading">{t("lang.popups.schedule_meeting.second_step.duration.title")}</h4>
-                        <ul className="durations">
-                           <li data-duration="5" onClick={(ev) => handleDurationClicked(ev)}>
-                              {t("lang.popups.schedule_meeting.second_step.duration.durations.five_min")}
-                           </li>
-                           <li data-duration="10" onClick={(ev) => handleDurationClicked(ev)}>
-                              {t("lang.popups.schedule_meeting.second_step.duration.durations.ten_min")}
-                           </li>
-                           <li data-duration="20" onClick={(ev) => handleDurationClicked(ev)}>
-                              {t("lang.popups.schedule_meeting.second_step.duration.durations.twenty_min")}
-                           </li>
-                        </ul>
-                        <div className="schedule_time">
-                           <h4 className="heading">{t("lang.popups.schedule_meeting.second_step.time.title")}</h4>
-                           <p className="desc">{t("lang.popups.schedule_meeting.second_step.time.caption")}</p>
-                           <div className="input_wrapper">
-                              <select
-                                 {...register("meeting_time", { required: t("lang.popups.schedule_meeting.second_step.time.unselected_time_mess") })}
-                                 type="time"
-                                 name="meeting_time"
-                                 id="meetingTime"
-                              >
-                                 {availableTimes && availableTimes.map((availTime) => <option value={availTime.from}>{availTime.from}</option>)}
-                              </select>
-                              <p className="error">{errors.meeting_time?.message}</p>
+                        <div className="meeting_container">
+                           <div className="meeting_info">
+                              <div className="meeting_title">
+                                 <div className="title">
+                                    <input
+                                       {...register("meeting_title", {
+                                          required: t("lang.popups.schedule_meeting.second_step.title.empty_title_mess"),
+                                          minLength: { value: 5, message: t("lang.popups.schedule_meeting.second_step.title.min_length_mess") },
+                                          maxLength: { value: 50, message: t("lang.popups.schedule_meeting.second_step.title.max_length_mess") },
+                                       })}
+                                       type="text"
+                                       name="meeting_title"
+                                       id="meetingTitle"
+                                       placeholder={t("lang.popups.schedule_meeting.second_step.title.title")}
+                                    />
+                                    <p className="error">{errors.meeting_title?.message}</p>
+                                 </div>
+                              </div>
+                              <div className="select_duration">
+                                 <h4 className="heading">{t("lang.popups.schedule_meeting.second_step.duration.title")}</h4>
+                                 <ul className="durations">
+                                    <li data-duration="5" onClick={(ev) => handleItemClicked(ev, setSelectedDuration, "5")}>
+                                       {t("lang.popups.schedule_meeting.second_step.duration.durations.five_min")}
+                                    </li>
+                                    <li data-duration="10" onClick={(ev) => handleItemClicked(ev, setSelectedDuration, "10")}>
+                                       {t("lang.popups.schedule_meeting.second_step.duration.durations.ten_min")}
+                                    </li>
+                                    <li data-duration="20" onClick={(ev) => handleItemClicked(ev, setSelectedDuration, "20")}>
+                                       {t("lang.popups.schedule_meeting.second_step.duration.durations.twenty_min")}
+                                    </li>
+                                 </ul>
+                              </div>
                            </div>
-                        </div>
 
-                        <div className="meeting_title">
-                           <h4 className="heading">{t("lang.popups.schedule_meeting.second_step.title.title")}</h4>
-                           <div className="title">
-                              <input
-                                 {...register("meeting_title", {
-                                    required: t("lang.popups.schedule_meeting.second_step.title.empty_title_mess"),
-                                    minLength: { value: 5, message: t("lang.popups.schedule_meeting.second_step.title.min_length_mess") },
-                                    maxLength: { value: 50, message: t("lang.popups.schedule_meeting.second_step.title.max_length_mess") },
+                           <div className="schedule_time">
+                              <h4 className="heading">{t("lang.popups.schedule_meeting.second_step.time.title")}</h4>
+                              <div className="availabilities">
+                                 {availableTimes.map((avail) => {
+                                    const availRemovedSec = avail[0].substring(0, avail[0].length - 3);
+                                    return (
+                                       <div onClick={(ev) => handleItemClicked(ev, setSelectedTime, avail[0])} className="availability">
+                                          {availRemovedSec}
+                                       </div>
+                                    );
                                  })}
-                                 type="text"
-                                 name="meeting_title"
-                                 id="meetingTitle"
-                              />
-                              <p className="error">{errors.meeting_title?.message}</p>
+                              </div>
                            </div>
                         </div>
 
-                        <div className="doctor_times">
-                           <div className="booked_times">
-                              <h5>{t("lang.popups.schedule_meeting.second_step.booked_times")}</h5>
-                              <ul className="unavailable_list">
-                                 {scheduledTimes && scheduledTimes.map((takenTime, i) => <TimeDuration key={i} from={takenTime.from} to={takenTime.to} />)}
-                              </ul>
-                           </div>
-                        </div>
                         <div className="submit-date">
                            <button id="scheduleMeetNextStage">{t("lang.popups.schedule_meeting.second_step.confirm_button")}</button>
                         </div>
